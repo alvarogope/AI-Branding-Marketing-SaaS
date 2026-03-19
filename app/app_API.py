3fd43312e4909cd5ccedd969a06c6bc6442e9f93
Alvaro Gomez <alvarogope99@gmail.com>
2026-03-19 17:47:49 +0000

    Replace HTTPException import from http.client with FastAPI

--- app/app_API.py
+++ app/app_API.py
@@ -1,4 +1,4 @@
-from http.client import HTTPException
+from fastapi import HTTPException
 from fastapi import FastAPI
 from app.main import generate_branding_snippet, generate_keywordsS, MAX_INPUT_LENGTH
 
